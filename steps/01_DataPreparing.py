2a6890aa44363113779041b30622aef53eb66827
Bart Dewancker <bart.dewancker@araani.com>
2023-06-15 20:49:41 +0200

    azure-sdk 1.36.0

--- steps/01_DataPreparing.py
+++ steps/01_DataPreparing.py
@@ -95,9 +95,8 @@ def checkDatasheets(ws):
 
     # Upload the directory with the checked datasheets as a new dataset.
     print(f'Uploading checked datasheets to {CHECKED_DATASET}')
-    print(f'checked_data_folder: {checked_data_folder}')
-    checked_datasheets = Dataset.File.upload_directory(src_dir = checked_data_folder,
-                                                       target = DataPath(datastore=ws.get_default_datastore(), path_on_datastore=f'processed_data/checked'),
+    checked_datasheets = Dataset.File.upload_directory(src_dir=checked_data_folder,
+                                                       target=DataPath(datastore=ws.get_default_datastore(), path_on_datastore=f'processed_data/checked'),
                                                        overwrite=True)
 
     # Register the checked datasheets as a new dataset.
